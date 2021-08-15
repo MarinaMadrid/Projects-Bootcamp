46400eec369dc6dc32fa44b0eed5241e014d5762
Marina Madrid García <74628083+MarinaMadrid@users.noreply.github.com>
2021-08-15 17:35:39 +0200

    delete desarrolladoras

--- Desarrolladoras/08FinalProject/api/main.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-// Defining the port on which the application will listen
-const port = 3000;
-
-// Starting the listening service
-app.listen(port, () => {
-    console.log('Escuchando en http://localhost:' + port);
-});
