07fe1e14bf49f6ba7517a10045207edac932cefd
Marina Madrid García <74628083+MarinaMadrid@users.noreply.github.com>
2021-09-02 15:39:02 +0200

    creating junit files and adding mockito dependency

---
+++ Proyectos/FinalProject02.2/m2-06-junit-events-app-m/src/test/java/tests/EventTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.example.Attendee;
+import com.example.Event;
+import com.example.EventType;
+import com.example.Speaker;
+import com.example.service.EventNotificationServiceImpl;
+
+class EventTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Proyectos/FinalProject02.2/m2-06-junit-events-app-m/src/test/java/tests/service/EventNotificationServiceImplTest.java
@@ -0,0 +1,26 @@
+package tests.service;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.Attendee;
+import com.example.Event;
+import com.example.EventType;
+import com.example.service.EventNotificationServiceImpl;
+
+class EventNotificationServiceImplTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
