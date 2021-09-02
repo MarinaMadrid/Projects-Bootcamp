3563071f7339c4638d7280daf4a6533a65e13cc7
Marina Madrid García <74628083+MarinaMadrid@users.noreply.github.com>
2021-09-02 21:39:15 +0200

    update HelloControllerTest

--- FinalProject02.1/m1-08-jersey-coche-test/src/test/java/tests/testsRest/HelloControllerTest.java
+++ FinalProject02.1/m1-08-jersey-coche-test/src/test/java/tests/testsRest/HelloControllerTest.java
@@ -4,11 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.example.demo.rest.HelloController;
+
 class HelloControllerTest {
+	HelloController helloController = new HelloController();
 
 	@Test
 	void helloTest() {
-		
+		String hello = helloController.hello();
+		assertEquals("Bienvenidos a Compramostucoche.es", hello);
 	}
 
 }
