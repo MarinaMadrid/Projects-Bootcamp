6b31ec31d4539ab1e58a8319b8dd94f073d8f4fc
Marina Madrid García <74628083+MarinaMadrid@users.noreply.github.com>
2021-09-03 13:53:34 +0200

    add announceTest

--- FinalProject02.2/m2-06-junit-events-app-m/src/test/java/tests/service/EventNotificationServiceImplTest.java
+++ FinalProject02.2/m2-06-junit-events-app-m/src/test/java/tests/service/EventNotificationServiceImplTest.java
@@ -1,19 +1,48 @@
 package tests.service;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.example.Attendee;
 import com.example.Event;
 import com.example.EventType;
 import com.example.service.EventNotificationServiceImpl;
 
+@ExtendWith(MockitoExtension.class)
 class EventNotificationServiceImplTest {
+	
+	EventNotificationServiceImpl eventNotificationServiceImpl;
+	Event event;
+	Attendee attendee;
+	
+	@Mock
+	EventNotificationServiceImpl mockEventNotificationServiceImpl;
+	Event mockEvent;
+	
+	@InjectMocks
+	Event inEvent;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@BeforeEach
+	void setUp() throws Exception {
+		eventNotificationServiceImpl = new EventNotificationServiceImpl();
+		attendee = new Attendee();
 	}
-
+	
+	@Test
+	@DisplayName("Announcing an event")
+	void announceTest() {
+		inEvent.notifyAssistants();
+		verify(mockEventNotificationServiceImpl, times(1)).announce(inEvent);
+	}
+	
+	
 }
