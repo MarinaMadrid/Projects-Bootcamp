686b198d3bfccb6b0c6cf5eff58ab897ca41285c
Marina Madrid García <74628083+MarinaMadrid@users.noreply.github.com>
2021-09-03 11:49:30 +0200

    add "addAttendeeTest"

--- FinalProject02.2/m2-06-junit-events-app-m/src/test/java/tests/EventTest.java
+++ FinalProject02.2/m2-06-junit-events-app-m/src/test/java/tests/EventTest.java
@@ -3,6 +3,7 @@ package tests;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.example.Attendee;
@@ -13,13 +14,19 @@ import com.example.service.EventNotificationServiceImpl;
 
 class EventTest {
 
+	Event event;
+
 	@BeforeEach
 	void setUp() throws Exception {
+		event = new Event();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Adding Attendee Test")
+	void testAddAttendeeTest() {
+		Attendee person1 = new Attendee(1L, "Marina", "marina@gmail.com");
+		event.addAttendee(person1);
+		assertEquals(1, event.getAttendees().size());
+		assertEquals("Marina", event.getAttendees().get(0).getNickname());
 	}
-
 }
