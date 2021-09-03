7d9f140f59055b97d8dde10c52757b29b9d7ada4
Marina Madrid García <74628083+MarinaMadrid@users.noreply.github.com>
2021-09-03 11:03:51 +0200

    changes

--- FinalProject02.2/m2-06-junit-events-app-m/src/test/java/tests/service/EventNotificationServiceImplTest.java
+++ FinalProject02.2/m2-06-junit-events-app-m/src/test/java/tests/service/EventNotificationServiceImplTest.java
@@ -1,15 +1,8 @@
 package tests.service;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.example.Attendee;
 import com.example.Event;
